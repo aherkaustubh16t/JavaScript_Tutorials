90f931bb49eece519f1ec992406c130bc4d9154f
aherkaustubh16t <aherkaustubhramesh16@gmail.com>
2025-02-12 12:54:48 +0530

    js primitives

---
+++ JS-primitivesobjects/index.js
@@ -0,0 +1,19 @@
+// nn bb ss u - Primitives in Js
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("567") + BigInt("3")
+let e = "Kaustubh"
+let f = Symbol("I am a nice symbol")
+let g
+console.log(a, b, c, d, e, f, g)
+console.log(typeof c)
+
+// Non Primitive Data Type - Objects in Js
+const item = {
+  "Kaustubh": true,
+  "Kaustubh2": false,
+  "Kaustubh3": 67,
+  "Kaustubh4": undefined
+}
+console.log(item["Shubh"])
