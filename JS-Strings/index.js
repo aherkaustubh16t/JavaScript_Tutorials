685754e8f55638e2c29f8e0c606083f55bee918d
aherkaustubh16t <aherkaustubhramesh16@gmail.com>
2025-02-15 20:59:38 +0530

    Strings

---
+++ JS-Strings/index.js
@@ -0,0 +1,21 @@
+let name = "Kaustubh"
+// console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+let friend = 'Sahil'
+console.log(friend)
+// let myfriend = "Shubh' // Dont do this
+// console.log(myfriend)
+
+// Template literals
+let boy1 = "Pushkar"
+let boy2 = "Kaustubh"
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+// let fruit = 'Bana\'na'
+// console.log(fruit)
+let fruit = "Bana\"na"
+console.log(fruit)
